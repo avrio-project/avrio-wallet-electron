204b9e16316a1f6792e8720b3a3b4ce0d69e04a6
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-06-10 08:54:23 +0100

    Update ws_config.js

--- src/js/ws_config.js
+++ src/js/ws_config.js
@@ -1,23 +1,23 @@
 var config = {};
 
 // self explanatory, your application name, descriptions, etc
-config.appName = 'Pengolin-WalletShell';
-config.appDescription = 'PengolinCoin Wallet';
-config.appSlogan = 'Slow and steady wins the race!';
-config.appId = 'lol.pengolincoin.walletshell';
-config.appGitRepo = 'https://github.com/pengolincoin/pengolin-wallet-electron';
+config.appName = 'AvrioWallet';
+config.appDescription = 'Avrio GUI Wallet';
+config.appSlogan = 'Tomorows money, today.';
+config.appId = 'tk.avrio.walletshell';
+config.appGitRepo = 'https://github.com/avrio-project/avrio-wallet-electron';
 
 // default port number for your daemon (e.g. PengolinCoind)
-config.daemonDefaultRpcPort = 17898;
+config.daemonDefaultRpcPort = 24534;
 
 // wallet file created by this app will have this extension
-config.walletFileDefaultExt = 'ewl';
+config.walletFileDefaultExt = 'aws';
 
 // change this to match your wallet service executable filename
-config.walletServiceBinaryFilename = 'pengolin-service';
+config.walletServiceBinaryFilename = 'avrio-service';
 
-// version on the bundled service (pengolin-service)
-config.walletServiceBinaryVersion = "2.0.0.1";
+// version on the bundled service (avrio-service)
+config.walletServiceBinaryVersion = "2.0.0.0";
 
 // config file format supported by wallet service, possible values:
 // ini -->  for turtle service (or its forks) version <= v0.8.3
@@ -25,44 +25,43 @@ config.walletServiceBinaryVersion = "2.0.0.1";
 config.walletServiceConfigFormat = "json";
 
 // default port number for your wallet service (e.g. pengolin-service)
-config.walletServiceRpcPort = 8979;
+config.walletServiceRpcPort = 24562;
 
 // block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
-config.blockExplorerUrl = 'http://peng-explorer.s2ii.xyz/?hash=[[TX_HASH]]#blockchain_transaction';
+config.blockExplorerUrl = 'http://aioexp.mine2.live/?hash=[[TX_HASH]]#blockchain_transaction';
 
 // default remote node to connect to, set this to a known reliable node for 'just works' user experience
-config.remoteNodeDefaultHost = '78.46.222.91';
+config.remoteNodeDefaultHost = '99.248.224.55';
 
 // remote node list update url, set to null if you don't have one
-config.remoteNodeListUpdateUrl = https://raw.githubusercontent.com/pengolincoin/pgo-nodes-json/master/nodes.json;
+config.remoteNodeListUpdateUrl = https://raw.githubusercontent.com/avrio-project/avrio-nodes-json/master/nodes.json;
 
 // fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
 config.remoteNodeListFallback = [
-  '78.46.222.91:17898',
-  '116.203.178.152:17898',
+  '99.248.224.55:24534',
 ];
 
 // your currency name
-config.assetName = 'PengolinCoin';
+config.assetName = 'Avrio';
 // your currency ticker
-config.assetTicker = 'PGO';
+config.assetTicker = 'AIO';
 // your currency address prefix, for address validation
-config.addressPrefix = 'PNGLNN';
+config.addressPrefix = 'aio';
 // standard wallet address length, for address validation
-config.addressLength = 101;
+config.addressLength = 98;
 // intergrated wallet address length, for address validation
-config.integratedAddressLength = 187;
+config.integratedAddressLength = 186;
 
 // minimum fee for sending transaction
-config.minimumFee = 0.00001;
+config.minimumFee = 0.025;
 // minimum amount for sending transaction
-config.mininumSend = 0.0001;
+config.mininumSend = 0.01;
 // default mixin/anonimity for transaction
 config.defaultMixin = 1;
 // to convert from atomic unit
-config.decimalDivisor = 100000000;
+config.decimalDivisor = 100000;
 // to represent human readable value
-config.decimalPlaces = 8;
+config.decimalPlaces = 6;
 
 // obfuscate address book entries, set to false if you want to save it in plain json file.
 // not for security because the encryption key is attached here
@@ -70,13 +69,7 @@ config.addressBookObfuscateEntries = true;
 // key use to obfuscate address book contents
 config.addressBookObfuscationKey = '78779fb00ca1b7170832a42de45142cf6c4b7f733fe6fba5';
 // initial/sample entries to fill new address book
-config.addressBookSampleEntries = [
-  {
-    name: 'PengolinCoin Wallet Donation',
-    address: 'PNGLNN2iFo8AR24SfySidHbL2846iFzCtT6jXhnqrqFZZL2cSd7T6ot4wswPyLAXEvVrCFdsEBf3UG6fuySBeAAFJXXJAMvBmAV4P',
-    paymentId: '',
-  }
-];
+config.addressBookSampleEntries = [];
 
 config.addressBookCipherConfig = {
   algorithm: 'aes-256-gcm',
